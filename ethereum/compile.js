83bc6aea84b92fc4237cb8637b4a3ef923cfd64b
Sam Wellander <samwellander@gmail.com>
2018-11-19 11:43:33 -0500

    add compile scripts

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,16 +2,22 @@ const fs = require('fs-extra');
 const path = require('path');
 const solc = require('solc');
 
+const buildPath = path.resolve(__dirname, 'build');
+
+//delete everything inside of build directory
+fs.removeSync(buildPath);
+
 const eventPath = path.resolve(__dirname, 'contracts', 'Event.sol');
 const source = fs.readFileSync(eventPath, 'utf8');
+const { contracts } = solc.compile(source, 1);
 
-const { contracts } = solc.compile(source, 2);
-const Event = contracts[":Event"];
-const EventFactory = contracts[":EventFactory"];
+//make sure that build dir exists, create it if not
+fs.ensureDirSync(buildPath);
 
-module.exports = {
-  Event,
-  EventFactory
+//write contracts to new files in build folder
+for (let contract in contracts) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.slice(1) + '.json'),
+    contracts[contract]
+  );
 }
-
-console.log(contracts);
