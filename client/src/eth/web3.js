851aeee44e84509310a438f4e8ad4ef5f5047d80
Sam Wellander <samwellander@gmail.com>
2018-11-28 15:02:45 -0500

    add some feedback for users without metamask

--- client/src/eth/web3.js
+++ client/src/eth/web3.js
@@ -3,7 +3,11 @@ import Web3 from 'web3';
 let web3;
 
 if (typeof window !== 'undefined') {
-  web3 = new Web3(window.web3.currentProvider)
+  if (typeof window.web3 !== 'undefined') {
+    web3 = new Web3(window.web3.currentProvider)
+  } else {
+    alert('Please log into MetaMask and reload page')
+  }
 } else {
   console.log('not in browser')
 }
