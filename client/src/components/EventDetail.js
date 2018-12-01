f9f6ca2f7188479fabbf874ea2b60d0e5c0f5953
Sam Wellander <samwellander@gmail.com>
2018-12-01 18:50:23 -0500

    only organizer can invite

--- client/src/components/EventDetail.js
+++ client/src/components/EventDetail.js
@@ -23,10 +23,16 @@ class EventDetail extends Component {
     this.props.history.push('/rsvp');
   }
   render() {
-    const { selectedInvite, selectedEvent, eventId } = this.props;
+    const { auth, selectedInvite, selectedEvent, eventId } = this.props;
     if (!selectedEvent) return <h3>Spinner...</h3>
     else {
       const { title, location, date, description, address, stake } = selectedEvent;
+
+      //auth user id is a string
+      const isOrganizer = +auth.user.id === selectedEvent.organizerId;
+      console.log('userId', auth.user.id)
+      console.log('organizerId', selectedEvent.organizerId)
+      console.log('isOrganizer', isOrganizer);
       return (
         <div>
           {/* update status to deployed if event has an address */}
@@ -45,8 +51,12 @@ class EventDetail extends Component {
           <p>{address}</p>
           <p>{stake} ETH</p>
 
-          <h3>Invite Guests</h3>
-          <InviteGuests />
+          {isOrganizer && (
+            <div>
+              <h3>Invite Guests</h3>
+              <InviteGuests />
+            </div>
+          )}
 
           <h3>Guests</h3>
 
@@ -58,12 +68,13 @@ class EventDetail extends Component {
   }
 }
 
-const mapStateToProps = ({ events, invites }, { match }) => {
+const mapStateToProps = ({ events, invites, auth }, { match }) => {
   const eventId = +match.params.id; //coerce string id to an int to match with data returned from sequelize
   const selectedInvite = invites.find(invite => invite.event.id === eventId);
   const allEvents = events.concat(invites.map(invite => invite.event));
   const selectedEvent = allEvents.find(event => event.id === eventId);
   return {
+    auth,
     selectedInvite,
     selectedEvent,
     eventId
