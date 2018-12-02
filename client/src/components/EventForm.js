3e9c303ac9753dcfabeee0d53d3c75bbfa8023b7
Sam Wellander <samwellander@gmail.com>
2018-12-02 11:50:54 -0500

    add location autocomplete

--- client/src/components/EventForm.js
+++ client/src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LocationSearchInput from './LocationSearchInput';
 
 export default class EventForm extends Component {
   state = {
@@ -8,9 +9,19 @@ export default class EventForm extends Component {
     date: '',
     description: '',
     stake: 0,
+    lat: 0,
+    lng: 0
   }
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleAddressSelect = ({ address, lat, lng }) => {
+    this.setState({
+      location: address,
+      lat,
+      lng
+    })
   }
   handleSubmit = e => {
     e.preventDefault();
@@ -18,61 +29,62 @@ export default class EventForm extends Component {
   }
   render() {
     return (
-      <div style={{width: '60vw', position: 'absolute', left: '20%', fontFamily: 'Andale Mono'}}>
+      <div style={{ width: '60vw', position: 'absolute', left: '20%', fontFamily: 'Andale Mono' }}>
         <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label><strong>Title</strong></label>
-          <input
-            onChange={this.handleChange}
-            value={this.state.title}
-            name="title"
-            type="text"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label><strong>Location</strong></label>
-          <input
+          <div className="form-group">
+            <label><strong>Title</strong></label>
+            <input
+              onChange={this.handleChange}
+              value={this.state.title}
+              name="title"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label><strong>Location</strong></label>
+            {/* <input
             onChange={this.handleChange}
             value={this.state.location}
             name="location"
             type="text"
             className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label><strong>Date</strong></label>
-          <input
-            onChange={this.handleChange}
-            value={this.state.date}
-            name="date"
-            type="date"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label><strong>Description</strong></label>
-          <input
-            onChange={this.handleChange}
-            value={this.state.description}
-            name="description"
-            type="text"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label><strong>Stake</strong></label>
-          <input
-            onChange={this.handleChange}
-            value={this.state.stake}
-            name="stake"
-            type="number"
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <button className="btn btn-primary" type="submit">Create</button>
-        </div>
+          /> */}
+            <LocationSearchInput selectAddress={this.handleAddressSelect} />
+          </div>
+          <div className="form-group">
+            <label><strong>Date</strong></label>
+            <input
+              onChange={this.handleChange}
+              value={this.state.date}
+              name="date"
+              type="date"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label><strong>Description</strong></label>
+            <input
+              onChange={this.handleChange}
+              value={this.state.description}
+              name="description"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label><strong>Stake</strong></label>
+            <input
+              onChange={this.handleChange}
+              value={this.state.stake}
+              name="stake"
+              type="number"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">Create</button>
+          </div>
         </form>
       </div>
     )
