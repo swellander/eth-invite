eccd14d237547a081bafeab6f5eced39ad48a59d
Sam Wellander <samwellander@gmail.com>
2018-12-02 15:25:29 -0500

    fix webcam syntax bug

--- client/src/components/CamCapture.js
+++ client/src/components/CamCapture.js
@@ -9,11 +9,9 @@ import isAtLocation from '../isAtLocation';
 
 const {
   addUserImageToCollection,
-  sendFacesToCollection,
-  searchFaces,
+  sendFacesToCollection, searchFaces,
   deleteFaces,
 } = require('../awsUtils/rekog');
-const { getEventImages } = require('../awsUtils/image');
 
 class CamCapture extends Component {
   constructor() {
@@ -26,8 +24,7 @@ class CamCapture extends Component {
 
   confirm = () => {
     const imageSrc = this.webcam.getScreenshot();
-    const bucketName = 'stakeevent' + this.props.match.params.eventId;
-    axios.post('/api/camera', { data: imageSrc, bucket: bucketName }).then(imageUrl => {
+    axios.post('/api/camera', { data: imageSrc }).then(imageUrl => {
       const regex = /[\w-]+.(jpg|png|jpeg)/;
       const imageName = regex.exec(imageUrl.data);
       const faceIdArray = [];
@@ -54,44 +51,45 @@ class CamCapture extends Component {
                   }
                 })
             }
-          })
-          .then(() => {
-            deleteFaces(faceIdArray);
-          });
-      });
+          } else {
+            console.log('faces not detected');
+            //do something like request to take another pic
+          }
+        })
+        .then(() => {
+          deleteFaces(faceIdArray);
+        });
+    });
   };
 
-  rsvp = async () => {
+  rsvp = () => {
     const imageSrc = this.webcam.getScreenshot();
     const userFaceId = this.props.auth.user.faceId;
 
-    axios
-      .post('/api/camera', { data: imageSrc, bucket: 'rsvpusers' })
-      .then(imageUrl => {
-        const regex = /[\w-]+.(jpg|png|jpeg)/;
-        const imageName = regex.exec(imageUrl.data);
+    axios.post('/api/camera', { data: imageSrc }).then(imageUrl => {
+      const regex = /[\w-]+.(jpg|png|jpeg)/;
+      const imageName = regex.exec(imageUrl.data);
 
-        if (userFaceId) {
-          deleteFaces([userFaceId]);
+      if (userFaceId) {
+        deleteFaces([userFaceId]);
+      }
+
+      addUserImageToCollection(imageName[0]).then(faces => {
+        if (faces.FaceRecords.length) {
+          axios.put(`/api/users/${this.props.auth.user.id}`, {
+            faceId: faces.FaceRecords[0].Face.FaceId,
+            imageUrl: imageUrl.data,
+          })
+            .then(() => {
+              //TODO: reload Auth with fresh user data, after faceId update is complete
+              this.props.refreshUser(this.props.auth.user.id);
+            })
+        } else {
+          console.log("the person's face was not detected");
+          //do something like request to take another pic
         }
-
-        addUserImageToCollection(imageName[0]).then(faces => {
-          if (faces.FaceRecords.length) {
-            axios
-              .put(`/api/users/${this.props.auth.user.id}`, {
-                faceId: faces.FaceRecords[0].Face.FaceId,
-                imageUrl: imageUrl.data,
-              })
-              .then(() => {
-                //TODO: reload Auth with fresh user data, after faceId update is complete
-                this.props.refreshUser(this.props.auth.user.id);
-              });
-          } else {
-            console.log("the person's face was not detected");
-            //do something like request to take another pic
-          }
-        });
       });
+    });
   };
 
   render() {
@@ -134,9 +132,8 @@ const mapStateToProps = ({ auth }) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    updateConfirmationStatus: (faceId, eventId) =>
-      dispatch(_updateConfirmationStatus(faceId, eventId)),
-    refreshUser: userId => dispatch(_refreshUser(userId)),
+    updateConfirmationStatus: (faceId, eventId) => dispatch(_updateConfirmationStatus(faceId, eventId)),
+    refreshUser: userId => dispatch(_refreshUser(userId))
   };
 };
 export default connect(
