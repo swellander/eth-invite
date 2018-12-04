59044014e2bf8032c389cc741ff637a767f5f16a
Sanjai S <sanjai.syamaprasad@gmail.com>
2018-12-04 07:25:29 -0500

    still working styling of  masonry.

--- client/src/components/ImageGallery.js
+++ client/src/components/ImageGallery.js
@@ -1,47 +1,50 @@
 import * as React from 'react';
-import Masonry from 'react-masonry-component';
 import { getEventImages } from '../awsUtils/image';
+import Masonry from 'react-masonry-css';
 
-const masonryOptions = {
-  transitionDuration: 0
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1,
 };
-
-const imagesLoadedOptions = { background: '.my-bg-image-el' }
 
 class Gallery extends React.Component {
   state = {
-    imageUrls: []
-  }
+    imageUrls: [],
+  };
   componentDidMount() {
     const { eventId } = this.props;
-    console.log('ID', eventId)
-    getEventImages(eventId)
-      .then(imageUrls => {
-        console.log('LOADED', imageUrls)
-        this.setState({ imageUrls })
-      });
+    console.log('ID', eventId);
+    getEventImages(eventId).then(imageUrls => {
+      console.log('LOADED', imageUrls);
+      this.setState({ imageUrls });
+    });
   }
   render() {
-    console.log('urls', this.state.imageUrls)
+    console.log('urls', this.state.imageUrls);
 
     return (
       <Masonry
-        className={'my-gallery-class'} // default ''
-        elementType={'ul'} // default 'div'
-        options={masonryOptions} // default {}
-        disableImagesLoaded={false} // default false
-        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-        imagesLoadedOptions={imagesLoadedOptions} // default {}
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
       >
-        {
-          this.state.imageUrls.map(function (url) {
-            return (
-              <div key={url} className="image-element-class">
-                <img src={url} />
-              </div>
-            );
-          })
-        }
+        {this.state.imageUrls.map(url => (
+          <img
+            style={{
+              width: 200,
+              height: getRandomSize(100, 300),
+            }}
+            src={url}
+            alt="Event pic"
+            key={url}
+          />
+        ))}
       </Masonry>
     );
   }
